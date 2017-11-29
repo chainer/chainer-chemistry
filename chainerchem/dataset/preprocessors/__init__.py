886d320158f64ebebafea12be6f66d5c6db98b20
Kenta OONO <oono@preferred.jp>
2017-11-29 14:06:18 +0900

    Fix import of ECFPPreprocessor

--- chainerchem/dataset/preprocessors/__init__.py
+++ chainerchem/dataset/preprocessors/__init__.py
@@ -8,8 +8,8 @@ from chainerchem.dataset.preprocessors.atomic_number_preprocessor import AtomicN
 from chainerchem.dataset.preprocessors.base_preprocessor import BasePreprocessor  # NOQA
 from chainerchem.dataset.preprocessors.common import construct_atomic_numbers  # NOQA
 from chainerchem.dataset.preprocessors.common import type_check_num_atoms  # NOQA
+from chainerchem.dataset.preprocessors.ecfp_preprocessor import ECFPPreprocessor  # NOQA
 from chainerchem.dataset.preprocessors.ggnn_preprocessor import GGNNPreprocessor  # NOQA
-from chainerchem.dataset.preprocessors.mol_preprocessor import ECFPPreprocessor  # NOQA
 from chainerchem.dataset.preprocessors.mol_preprocessor import MolFeatureExtractFailure  # NOQA
 from chainerchem.dataset.preprocessors.mol_preprocessor import MolPreprocessor  # NOQA
 from chainerchem.dataset.preprocessors.nfp_preprocessor import NFPPreprocessor  # NOQA
