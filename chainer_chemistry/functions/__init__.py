f5d115508bf7c5a206e5af6fafd04508e488541c
Akira Naruse <anaruse@nvidia.com>
2018-02-15 14:08:53 +0900

    Remove unused function and link

--- chainer_chemistry/functions/__init__.py
+++ chainer_chemistry/functions/__init__.py
@@ -1,2 +1,1 @@
 from chainer_chemistry.functions.matmul import matmul  # NOQA
-from chainer_chemistry.functions.graph_convolution import graph_convolution  # NOQA
--- chainer_chemistry/links/__init__.py
+++ chainer_chemistry/links/__init__.py
@@ -1,8 +1,6 @@
 from chainer_chemistry.links import embed_atom_id  # NOQA
 from chainer_chemistry.links import graph_linear  # NOQA
-from chainer_chemistry.links import graph_convolution  # NOQA
 
 from chainer_chemistry.links.embed_atom_id import EmbedAtomID  # NOQA
 from chainer_chemistry.links.graph_linear import GraphLinear  # NOQA
 from chainer_chemistry.links.graph_batch_normalization import GraphBatchNormalization  # NOQA
-from chainer_chemistry.links.graph_convolution import GraphConvolution  # NOQA
