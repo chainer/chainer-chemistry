5a1c698aa8b2e27af4733e9e269bc2d4dfaf97a2
mottodora <motoki@dna.bio.keio.ac.jp>
2018-02-23 00:37:07 +0900

    return smiles by parser parse in sdf file parser

--- chainer_chemistry/dataset/parsers/sdf_file_parser.py
+++ chainer_chemistry/dataset/parsers/sdf_file_parser.py
@@ -26,26 +26,27 @@ class SDFFileParser(BaseFileParser):
         self.labels = labels
         self.postprocess_label = postprocess_label
         self.postprocess_fn = postprocess_fn
-        self.smiles = None
         self.logger = logger or getLogger(__name__)
 
-    def parse(self, filepath, retain_smiles=False):
+    def parse(self, filepath, return_smiles=True):
         """parse sdf file using `preprocessor`
 
         Note that label is extracted from preprocessor's method.
 
         Args:
             filepath (str): file path to be parsed.
-            retain_smiles (bool): If set to True, smiles list is saved to
-                `smiles` property.
+            return_smiles (bool): If set to True, this function returns
+                preprocessed dataset and smiles list.
+                If set to False, this function returns preprocessed dataset and
+                `None`.
 
-        Returns: Dataset
+        Returns (tuple): Dataset, 1-d numpy array with dtype=object(string)
+            which is a vector of smiles for each example or None.
 
         """
         logger = self.logger
         pp = self.preprocessor
-        if retain_smiles:
-            self.smiles = []  # Initialize
+        smiles_list = [] if return_smiles else None # initialize
 
         if isinstance(pp, MolPreprocessor):
             mol_supplier = Chem.SDMolSupplier(filepath)
@@ -88,9 +89,9 @@ class SDFFileParser(BaseFileParser):
                             num_features += 1
                         features = [[] for _ in range(num_features)]
 
-                    if retain_smiles:
+                    if return_smiles:
                         assert standardized_smiles == Chem.MolToSmiles(mol)
-                        self.smiles.append(standardized_smiles)
+                        smiles_list.append(standardized_smiles)
                 except MolFeatureExtractionError as e:
                     # This is expected error that extracting feature failed,
                     # skip this molecule.
@@ -132,21 +133,8 @@ class SDFFileParser(BaseFileParser):
         if isinstance(result, tuple):
             if self.postprocess_fn is not None:
                 result = self.postprocess_fn(*result)
-            return NumpyTupleDataset(*result)
+            return NumpyTupleDataset(*result), smiles_list
         else:
             if self.postprocess_fn is not None:
                 result = self.postprocess_fn(result)
-            return NumpyTupleDataset(result)
-
-    def get_smiles(self):
-        """get smiles array
-
-        Returns (numpy.ndarray): 1-d numpy array with dtype=object (string),
-            which is a vector of smiles for each example.
-
-        """
-        if self.smiles is None:
-            self.logger.warning('smiles is None, please execute parse method '
-                                'with retrain_smiles=True.')
-            return None
-        return numpy.array(self.smiles)
+            return NumpyTupleDataset(result), smiles_list
