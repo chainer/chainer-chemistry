297ea7cb7be002641e152e1b1177d7f9b67aefff
mottodora <motoki@dna.bio.keio.ac.jp>
2018-02-23 00:48:34 +0900

    fix tests

--- tests/dataset_tests/parsers_tests/test_csv_file_parser.py
+++ tests/dataset_tests/parsers_tests/test_csv_file_parser.py
@@ -52,11 +52,12 @@ def check_features(actual, expect_input_features, expect_label):
     assert actual[-1] == expect_label
 
 
-def test_csv_file_parser(csv_file, mols):
+def test_csv_file_parser_not_return_smiles(csv_file, mols):
     preprocessor = NFPPreprocessor()
     parser = CSVFileParser(preprocessor, smiles_col='smiles')
-    dataset = parser.parse(csv_file)
+    dataset = parser.parse(csv_file, return_smiles=False)
     assert len(dataset) == 2
+    assert smiles == None
 
     # As we want test CSVFileParser, we assume
     # NFPPreprocessor works as documented.
@@ -67,12 +68,11 @@ def test_csv_file_parser(csv_file, mols):
     check_input_features(dataset[1], expect)
 
 
-def test_csv_file_parser_retain_smiles(csv_file, mols, label_a):
+def test_csv_file_parser_return_smiles(csv_file, mols, label_a):
     """test `labels` option and retain_smiles=True."""
     preprocessor = NFPPreprocessor()
     parser = CSVFileParser(preprocessor, labels='labelA', smiles_col='smiles')
-    dataset = parser.parse(csv_file, retain_smiles=True)
-    smiles = parser.get_smiles()
+    dataset, smiles = parser.parse(csv_file, return_smiles=True)
     assert len(dataset) == 2
 
     # As we want test CSVFileParser, we assume
