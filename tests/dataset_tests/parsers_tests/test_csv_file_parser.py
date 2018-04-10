9c822ec913c032c210b34f5ceea4ec44ae0235b0
corochann <corochannz@gmail.com>
2018-04-10 13:41:50 +0900

    Add target_index to CSV parser

--- tests/dataset_tests/parsers_tests/test_csv_file_parser.py
+++ tests/dataset_tests/parsers_tests/test_csv_file_parser.py
@@ -13,7 +13,8 @@ from chainer_chemistry.dataset.preprocessors import NFPPreprocessor
 def mol_smiles():
     mol_smiles1 = 'CN=C=O'
     mol_smiles2 = 'Cc1ccccc1'
-    return [mol_smiles1, mol_smiles2]
+    mol_smiles3 = 'CC1=CC2CC(CC1)O2'
+    return [mol_smiles1, mol_smiles2, mol_smiles3]
 
 
 @pytest.fixture
@@ -23,7 +24,7 @@ def mols(mol_smiles):
 
 @pytest.fixture()
 def label_a():
-    return [2.1, 5.3]
+    return [2.1, 5.3, -1.2]
 
 
 @pytest.fixture()
@@ -59,16 +60,14 @@ def test_csv_file_parser_not_return_smiles(csv_file, mols):
     result = parser.parse(csv_file, return_smiles=False)
     dataset = result['dataset']
     smiles = result['smiles']
-    assert len(dataset) == 2
+    assert len(dataset) == 3
     assert smiles is None
 
     # As we want test CSVFileParser, we assume
     # NFPPreprocessor works as documented.
-    expect = preprocessor.get_input_features(mols[0])
-    check_input_features(dataset[0], expect)
-
-    expect = preprocessor.get_input_features(mols[1])
-    check_input_features(dataset[1], expect)
+    for i in range(3):
+        expect = preprocessor.get_input_features(mols[i])
+        check_input_features(dataset[i], expect)
 
 
 def test_csv_file_parser_return_smiles(csv_file, mols, label_a):
@@ -78,15 +77,13 @@ def test_csv_file_parser_return_smiles(csv_file, mols, label_a):
     result = parser.parse(csv_file, return_smiles=True)
     dataset = result['dataset']
     smiles = result['smiles']
-    assert len(dataset) == 2
+    assert len(dataset) == 3
 
     # As we want test CSVFileParser, we assume
     # NFPPreprocessor works as documented.
-    expect0 = preprocessor.get_input_features(mols[0])
-    check_features(dataset[0], expect0, label_a[0])
-
-    expect1 = preprocessor.get_input_features(mols[1])
-    check_features(dataset[1], expect1, label_a[1])
+    for i in range(3):
+        expect = preprocessor.get_input_features(mols[i])
+        check_features(dataset[i], expect, label_a[i])
 
     # check smiles array
     assert type(smiles) == numpy.ndarray
@@ -94,6 +91,32 @@ def test_csv_file_parser_return_smiles(csv_file, mols, label_a):
     assert len(smiles) == len(dataset)
     assert smiles[0] == 'CN=C=O'
     assert smiles[1] == 'Cc1ccccc1'
+    assert smiles[2] == 'CC1=CC2CC(CC1)O2'
+
+
+def test_csv_file_parser_target_index(csv_file, mols, label_a):
+    """test `labels` option and retain_smiles=True."""
+    preprocessor = NFPPreprocessor()
+    parser = CSVFileParser(preprocessor, labels='labelA', smiles_col='smiles')
+    result = parser.parse(csv_file, return_smiles=True, target_index=[0, 2])
+    dataset = result['dataset']
+    smiles = result['smiles']
+    assert len(dataset) == 2
+
+    # As we want test CSVFileParser, we assume
+    # NFPPreprocessor works as documented.
+    expect = preprocessor.get_input_features(mols[0])
+    check_features(dataset[0], expect, label_a[0])
+
+    expect = preprocessor.get_input_features(mols[2])
+    check_features(dataset[1], expect, label_a[2])
+
+    # check smiles array
+    assert type(smiles) == numpy.ndarray
+    assert smiles.ndim == 1
+    assert len(smiles) == len(dataset)
+    assert smiles[0] == 'CN=C=O'
+    assert smiles[1] == 'CC1=CC2CC(CC1)O2'
 
 
 if __name__ == '__main__':
