5240c5a5b51e2d4ddf5e9f77f4af2f0288777160
corochann <acc1ssnn9terias@gmail.com>
2018-06-13 19:07:31 +0900

    use bash -x

--- examples/test_examples.sh
+++ examples/test_examples.sh
@@ -7,16 +7,16 @@ echo Using gpu ${gpu}
 
 # Tox21
 echo --- Testing Tox21 ---
-cd tox21 && ./test_tox21.sh ${gpu} && cd ..
+cd tox21 && bash -x test_tox21.sh ${gpu} && cd ..
 
 # QM9
 echo --- Testing QM9 ---
-cd qm9 && ./test_qm9.sh ${gpu} && cd ..
+cd qm9 && bash -x test_qm9.sh ${gpu} && cd ..
 
 # Own dataset
 echo --- Testing Own dataset ---
-cd own_dataset && ./test_own_dataset.sh ${gpu} && cd ..
+cd own_dataset && bash -x test_own_dataset.sh ${gpu} && cd ..
 
 # MolNet
 echo --- Testing MolNet dataset ---
-cd molnet && ./test_molnet.sh ${gpu} && cd ..
+cd molnet && bash -x test_molnet.sh ${gpu} && cd ..
