454f5679aabff7865dc5210684b45119162343c5
mottodora <mottodora@gmail.com>
2018-06-21 10:52:11 +0900

    fix tests

--- tests/functions_tests/test_mean_absolute_error.py
+++ tests/functions_tests/test_mean_absolute_error.py
@@ -1,82 +1,150 @@
-import unittest
-
 import numpy
+import pytest
 
 import chainer
 from chainer import cuda
-from chainer import functions
 from chainer import gradient_check
-from chainer import testing
-from chainer.testing import attr
+
+import chainer_chemistry
 
 
-class TestMeanAbsoluteError(unittest.TestCase):
-
-    def setUp(self):
-        self.x0 = numpy.random.uniform(-1, 1, (4, 3)).astype(numpy.float32)
-        # Add sufficient margin to prevent computational error
-        diff = numpy.random.uniform(-1, 1, (4, 3)).astype(numpy.float32)
-        diff[abs(diff) < 0.01] = 0.5
-        self.x1 = self.x0 + diff
-        self.gy = numpy.random.uniform(-1, 1, ()).astype(numpy.float32)
-        self.ggx0 = numpy.random.uniform(-1, 1, (4, 3)).astype(numpy.float32)
-        self.ggx1 = numpy.random.uniform(-1, 1, (4, 3)).astype(numpy.float32)
-
-    def check_forward(self, x0_data, x1_data):
-        x0 = chainer.Variable(x0_data)
-        x1 = chainer.Variable(x1_data)
-        loss = functions.mean_absolute_error(x0, x1)
-        loss_value = cuda.to_cpu(loss.data)
-
-        assert loss_value.dtype == numpy.float32
-        assert loss_value.shape == ()
-
-        # Compute expected value
-        loss_expect = 0.
-        for i in numpy.ndindex(self.x0.shape):
-            loss_expect += abs(self.x0[i] - self.x1[i])
-        loss_expect /= self.x0.size
-
-        assert round(loss_expect - loss_value, 5) == 0
-
-    def test_forward_cpu(self):
-        self.check_forward(self.x0, self.x1)
-
-    @attr.gpu
-    def test_forward_gpu(self):
-        self.check_forward(cuda.to_gpu(self.x0), cuda.to_gpu(self.x1))
-
-    def check_backward(self, x0_data, x1_data):
-        gradient_check.check_backward(
-            functions.mean_absolute_error, (x0_data, x1_data), None, eps=1e-2)
-
-    def test_backward_cpu(self):
-        self.check_backward(self.x0, self.x1)
-
-    @attr.gpu
-    def test_backward_gpu(self):
-        self.check_backward(cuda.to_gpu(self.x0), cuda.to_gpu(self.x1))
-
-    def check_double_backward(self, x0_data, x1_data, gy_data, ggx0_data,
-                              ggx1_data):
-        def f(*xs):
-            y = chainer.functions.mean_absolute_error(*xs)
-            return y * y
-
-        gradient_check.check_double_backward(
-            f, (x0_data, x1_data), gy_data, (ggx0_data, ggx1_data), eps=1e-2)
-
-    def test_double_backward_cpu(self):
-        self.check_double_backward(
-            self.x0, self.x1, self.gy, self.ggx0, self.ggx1)
-
-    @attr.gpu
-    def test_double_backward_gpu(self):
-        self.check_double_backward(cuda.to_gpu(self.x0),
-                                   cuda.to_gpu(self.x1),
-                                   cuda.to_gpu(self.gy),
-                                   cuda.to_gpu(self.ggx0),
-                                   cuda.to_gpu(self.ggx1))
+@pytest.fixture
+def inputs():
+    x0 = numpy.random.uniform(-1, 1, (4, 3)).astype(numpy.float32)
+    # Add sufficient margin to prevent computational error
+    diff = numpy.random.uniform(-1, 1, (4, 3)).astype(numpy.float32)
+    diff[abs(diff) < 0.01] = 0.5
+    x1 = x0 + diff
+    x2 = numpy.asarray([[0.3, numpy.nan, 0.2],
+                        [numpy.nan, 0.1, 0.5],
+                        [0.9, 0.7, numpy.nan],
+                        [0.2, -0.3, 0.4]]).astype(numpy.float32)
+    return x0, x1, x2
 
 
-testing.run_module(__name__, __file__)
+@pytest.fixture
+def grads():
+    gy = numpy.random.uniform(-1, 1, ()).astype(numpy.float32)
+    ggx0 = numpy.random.uniform(-1, 1, (4, 3)).astype(numpy.float32)
+    ggx1 = numpy.random.uniform(-1, 1, (4, 3)).astype(numpy.float32)
+    return gy, ggx0, ggx1
+
+
+def check_forward(inputs):
+    x0_data, x1_data, _ = inputs
+    x0 = chainer.Variable(x0_data)
+    x1 = chainer.Variable(x1_data)
+    loss = chainer_chemistry.functions.mean_absolute_error(x0, x1)
+    loss_value = cuda.to_cpu(loss.data)
+    assert loss.dtype == numpy.float32
+    assert loss_value.shape == ()
+
+    loss_expect = 0.
+    for i in numpy.ndindex(x0_data.shape):
+        loss_expect += abs((x0_data[i] - x1_data[i]))
+    loss_expect /= x0_data.size
+    assert pytest.approx(loss_value, loss_expect)
+
+
+def check_forward_ignore_nan(inputs):
+    x0_data, _, x2_data = inputs
+    x0 = chainer.Variable(x0_data)
+    x2 = chainer.Variable(x2_data)
+    loss = chainer_chemistry.functions.mean_absolute_error(x0, x2,
+                                                           ignore_nan=True)
+    loss_value = cuda.to_cpu(loss.data)
+    assert loss.dtype == numpy.float32
+    assert loss_value.shape == ()
+
+    loss_expect = 0.
+    x0_data = cuda.to_cpu(x0_data)
+    x2_data = cuda.to_cpu(x2_data)
+    nan_mask = numpy.invert(numpy.isnan(x2_data)).astype(x2_data.dtype)
+    for i in numpy.ndindex(x0_data.shape):
+        loss_expect += abs(x0_data[i] -
+                           numpy.nan_to_num(x2_data[i])) * nan_mask[i]
+    loss_expect /= x0_data.size
+    assert numpy.allclose(loss_value, loss_expect)
+
+
+def check_forward_ignore_nan_with_nonnan_value(inputs):
+    x0_data, x1_data, _ = inputs
+    x0 = chainer.Variable(x0_data)
+    x1 = chainer.Variable(x1_data)
+    loss = chainer_chemistry.functions.mean_absolute_error(x0, x1,
+                                                           ignore_nan=True)
+    loss_value = cuda.to_cpu(loss.data)
+    assert loss.dtype == numpy.float32
+    assert loss_value.shape == ()
+
+    loss_expect = 0.
+    x0_data = cuda.to_cpu(x0_data)
+    x1_data = cuda.to_cpu(x1_data)
+    nan_mask = numpy.invert(numpy.isnan(x1_data)).astype(x1_data.dtype)
+    for i in numpy.ndindex(x0_data.shape):
+        loss_expect += abs(x0_data[i] -
+                           numpy.nan_to_num(x1_data[i])) * nan_mask[i]
+    loss_expect /= x0_data.size
+    assert numpy.allclose(loss_value, loss_expect)
+
+
+def test_forward_cpu(inputs):
+    check_forward(inputs)
+    check_forward_ignore_nan(inputs)
+    check_forward_ignore_nan_with_nonnan_value(inputs)
+
+
+def test_forward_gpu(inputs):
+    x0, x1, x2 = inputs
+    check_forward((cuda.to_gpu(x0), cuda.to_gpu(x1), None))
+    check_forward_ignore_nan((cuda.to_gpu(x0), None, cuda.to_gpu(x2)))
+
+
+def check_backward(inputs):
+    x0_data, x1_data, _ = inputs
+    gradient_check.check_backward(
+        chainer_chemistry.functions.mean_absolute_error,
+        (x0_data, x1_data), None, eps=1e-2)
+
+
+def check_backward_ignore_nan(inputs):
+    x0_data, _, x2_data = inputs
+
+    def func(x0, x1):
+        return chainer_chemistry.functions.mean_absolute_error(x0, x1,
+                                                               ignore_nan=True)
+    gradient_check.check_backward(func, (x0_data, x2_data), None, eps=1e-2)
+
+
+def test_backward_cpu(inputs):
+    check_backward(inputs)
+    check_backward_ignore_nan(inputs)
+
+
+@pytest.mark.gpu
+def test_backward_gpu(inputs):
+    x0, x1, x2 = inputs
+    check_backward((cuda.to_gpu(x0), cuda.to_gpu(x1), None))
+    check_backward_ignore_nan((cuda.to_gpu(x0), None, cuda.to_gpu(x2)))
+
+
+def check_double_backward(inputs, grads):
+    x0, x1, _ = inputs
+    gy, ggx0, ggx1 = grads
+    gradient_check.check_double_backward(
+        chainer_chemistry.functions.mean_absolute_error,
+        (x0, x1), gy, (ggx0, ggx1))
+
+
+def check_double_backward_ignore_nan(inputs, grads):
+    x0, _, x2 = inputs
+    gy, ggx0, ggx1 = grads
+
+    def func(x0, x1):
+        return chainer_chemistry.functions.mean_absolute_error(x0, x1,
+                                                               ignore_nan=True)
+    gradient_check.check_double_backward(func, (x0, x2), gy, (ggx0, ggx1))
+
+
+if __name__ == '__main__':
+    pytest.main([__file__, '-v'])
