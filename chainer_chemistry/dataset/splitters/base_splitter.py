db4cd3651a640cdbe664eee637ba02dc9101967c
Motoki Abe <mottodora@gmail.com>
2018-06-23 10:15:15 +0900

    add train valid split

--- chainer_chemistry/dataset/splitters/base_splitter.py
+++ chainer_chemistry/dataset/splitters/base_splitter.py
@@ -12,3 +12,15 @@ class BaseSplitter(object):
             return train_inds, valid_inds, test_inds
         else:
             return dataset[train_inds], dataset[valid_inds], dataset[test_inds]
+
+    def train_valid_split(self, dataset, frac_train=.9, frac_valid=.1,
+                          seed=None, return_index=True):
+        train_inds, valid_inds, test_inds = self.split(dataset,
+                                                       frac_train=frac_train,
+                                                       frac_valid=frac_valid,
+                                                       frac_test=0)
+        assert len(test_inds) == 0
+        if return_index:
+            return train_inds, valid_inds
+        else:
+            return dataset[train_inds], dataset[valid_inds]
