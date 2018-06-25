c5ea2a56b6549e4f2307df661e69f51907aa36b2
mottodora <mottodora@gmail.com>
2018-06-25 11:08:09 +0900

    add converter

--- chainer_chemistry/dataset/splitters/base_splitter.py
+++ chainer_chemistry/dataset/splitters/base_splitter.py
@@ -1,6 +1,19 @@
 from chainer_chemistry.datasets.numpy_tuple_dataset import NumpyTupleDataset
 
 
+def converter_default(dataset, indices):
+    return dataset[indices]
+
+
+def converter_numpy_tuple_dataset(dataset, indices):
+    return NumpyTupleDataset(*dataset.features[indices])
+
+
+converter_dict = {
+    NumpyTupleDataset: converter_numpy_tuple_dataset
+}
+
+
 class BaseSplitter(object):
     def k_fold_split(self, dataset, k):
         raise NotImplementedError
@@ -9,7 +22,11 @@ class BaseSplitter(object):
         raise NotImplementedError
 
     def train_valid_test_split(self, dataset, frac_train=.8, frac_valid=.1,
-                               frac_test=.1, seed=None, return_index=True):
+                               frac_test=.1, seed=None, return_index=True,
+                               converter=None):
+        if converter is None:
+            converter = converter_dict.get(type(dataset), converter_default)
+
         train_inds, valid_inds, test_inds = self._split(dataset,
                                                         frac_train=frac_train,
                                                         frac_valid=frac_valid,
