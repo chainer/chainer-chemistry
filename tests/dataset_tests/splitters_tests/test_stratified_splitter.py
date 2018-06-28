6328bbe669876870d0fea0203523a22b52597e61
mottodora <mottodora@gmail.com>
2018-06-28 15:27:25 +0900

    add unit tests for regression dataset

--- tests/dataset_tests/splitters_tests/test_stratified_splitter.py
+++ tests/dataset_tests/splitters_tests/test_stratified_splitter.py
@@ -9,8 +9,15 @@ from chainer_chemistry.datasets import NumpyTupleDataset
 def cls_dataset():
     a = numpy.random.random((30, 10))
     b = numpy.random.random((30, 8))
-    # c = numpy.random.randint(2, size=(30, 3))
     c = numpy.concatenate([numpy.zeros(20), numpy.ones(10)]).astype(numpy.int)
+    return NumpyTupleDataset(a, b, c)
+
+
+@pytest.fixture
+def reg_dataset():
+    a = numpy.random.random((100, 10))
+    b = numpy.random.random((100, 8))
+    c = numpy.arange(100).astype(numpy.float)
     return NumpyTupleDataset(a, b, c)
 
 
@@ -30,6 +37,24 @@ def test_classification_split(cls_dataset):
     assert train_ind.shape[0] == 15
     assert valid_ind.shape[0] == 9
     assert test_ind.shape[0] == 6
+
+
+def test_regression_split(reg_dataset):
+    splitter = StratifiedSplitter()
+    train_ind, valid_ind, test_ind = splitter._split(reg_dataset)
+    assert type(train_ind) == numpy.ndarray
+    assert train_ind.shape[0] == 80
+    assert valid_ind.shape[0] == 10
+    assert test_ind.shape[0] == 10
+
+    train_ind, valid_ind, test_ind = splitter._split(reg_dataset,
+                                                     frac_train=0.5,
+                                                     frac_valid=0.3,
+                                                     frac_test=0.2)
+    assert type(train_ind) == numpy.ndarray
+    assert train_ind.shape[0] == 50
+    assert valid_ind.shape[0] == 30
+    assert test_ind.shape[0] == 20
 
 
 def test_train_valid_test_classification_split(cls_dataset):
@@ -57,7 +82,32 @@ def test_train_valid_test_classification_split_return_dataset(cls_dataset):
     assert (test.features[:, -1] == 1).sum() == 1
 
 
-def test_train_valid_split(cls_dataset):
+def test_train_valid_test_regression_split(reg_dataset):
+    splitter = StratifiedSplitter()
+    train_ind, valid_ind, test_ind =\
+        splitter.train_valid_test_split(reg_dataset)
+    assert type(train_ind) == numpy.ndarray
+    assert train_ind.shape[0] == 80
+    assert valid_ind.shape[0] == 10
+    assert test_ind.shape[0] == 10
+
+
+def test_train_valid_test_regression_split_return_dataset(reg_dataset):
+    splitter = StratifiedSplitter()
+    train, valid, test = splitter.train_valid_test_split(reg_dataset,
+                                                         return_index=False)
+    assert type(train) == NumpyTupleDataset
+    assert type(valid) == NumpyTupleDataset
+    assert type(test) == NumpyTupleDataset
+    assert len(train) == 80
+    assert len(valid) == 10
+    assert len(test) == 10
+    assert 47.5 < train.features[:, -1].mean() < 52.5
+    assert 47.5 < valid.features[:, -1].mean() < 52.5
+    assert 47.5 < test.features[:, -1].mean() < 52.5
+
+
+def test_train_valid_classification_split(cls_dataset):
     splitter = StratifiedSplitter()
     train_ind, valid_ind = splitter.train_valid_split(cls_dataset)
     assert type(train_ind) == numpy.ndarray
@@ -65,7 +115,7 @@ def test_train_valid_split(cls_dataset):
     assert valid_ind.shape[0] == 3
 
 
-def test_train_valid_split_return_dataset(cls_dataset):
+def test_train_valid_classification_split_return_dataset(cls_dataset):
     splitter = StratifiedSplitter()
     train, valid = splitter.train_valid_split(cls_dataset, return_index=False)
     assert type(train) == NumpyTupleDataset
@@ -74,3 +124,22 @@ def test_train_valid_split_return_dataset(cls_dataset):
     assert len(valid) == 3
     assert (train.features[:, -1] == 1).sum() == 9
     assert (valid.features[:, -1] == 1).sum() == 1
+
+
+def test_train_valid_regression_split(reg_dataset):
+    splitter = StratifiedSplitter()
+    train_ind, valid_ind = splitter.train_valid_split(reg_dataset)
+    assert type(train_ind) == numpy.ndarray
+    assert train_ind.shape[0] == 90
+    assert valid_ind.shape[0] == 10
+
+
+def test_train_valid_regression_split_return_dataset(reg_dataset):
+    splitter = StratifiedSplitter()
+    train, valid = splitter.train_valid_split(reg_dataset, return_index=False)
+    assert type(train) == NumpyTupleDataset
+    assert type(valid) == NumpyTupleDataset
+    assert len(train) == 90
+    assert len(valid) == 10
+    assert 47.5 < train.features[:, -1].mean() < 52.5
+    assert 47.5 < valid.features[:, -1].mean() < 52.5
