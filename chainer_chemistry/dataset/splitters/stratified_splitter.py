1d6b05d07d1b794689dfe88175c7ff420d07cc29
mottodora <mottodora@gmail.com>
2018-06-28 17:54:46 +0900

    add documents

--- chainer_chemistry/dataset/splitters/stratified_splitter.py
+++ chainer_chemistry/dataset/splitters/stratified_splitter.py
@@ -5,6 +5,19 @@ from chainer_chemistry.dataset.splitters.base_splitter import BaseSplitter
 from chainer_chemistry.datasets import NumpyTupleDataset
 
 
+def _approximate_mode(class_counts, n_draws):
+    n_class = len(class_counts)
+    continuous = class_counts * n_draws / class_counts.sum()
+    floored = numpy.floor(continuous)
+    assert n_draws // n_class == floored.sum() // n_class
+    n_remainder = int(n_draws - floored.sum())
+    remainder = continuous - floored
+    inds = numpy.argsort(remainder)[:n_remainder]
+    floored[inds] += 1
+    assert n_draws == floored.sum()
+    return floored.astype(numpy.int)
+
+
 class StratifiedSplitter(BaseSplitter):
     """Class for doing stratified data splits."""
 
@@ -14,7 +27,6 @@ class StratifiedSplitter(BaseSplitter):
                                           1.)
 
         seed = kwargs.get('seed', None)
-        # TODO: feature?
         labels_feature_id = kwargs.get('labels_feature_id', -1)
         task_id = kwargs.get('task_id', 0)
 
@@ -76,16 +88,101 @@ class StratifiedSplitter(BaseSplitter):
             rng.permutation(valid_index),\
             rng.permutation(test_index),
 
+    def train_valid_test_split(self, dataset, labels_feature_id=-1, task_id=0,
+                               frac_train=0.8, frac_valid=0.1, frac_test=0.1,
+                               converter=None, return_index=True, seed=None):
+        """Generate indices by stratified splittting dataset into train, valid
+        and test set.
 
-def _approximate_mode(class_counts, n_draws):
-    n_class = len(class_counts)
-    continuous = class_counts * n_draws / class_counts.sum()
-    floored = numpy.floor(continuous)
-    assert n_draws // n_class == floored.sum() // n_class
-    # TODO: add remainder
-    n_remainder = int(n_draws - floored.sum())
-    remainder = continuous - floored
-    inds = numpy.argsort(remainder)[:n_remainder]
-    floored[inds] += 1
-    assert n_draws == floored.sum()
-    return floored.astype(numpy.int)
+        Args:
+            dataset(NumpyTupleDataset, numpy.ndarray):
+                Dataset.
+            labels_feature_id(int):
+                Dataset feature ID in NumpyTupleDataset.
+            task_id(int):
+                Target task number for stratification.
+            seed (int):
+                Random seed.
+            frac_train(float):
+                Fraction of dataset put into training data.
+            frac_valid(float):
+                Fraction of dataset put into validation data.
+            converter(callable):
+            return_index(bool):
+                If `True`, this function returns only indexes. If `False`, this
+                function returns splitted dataset.
+
+        Returns:
+            SplittedDataset(tuple):
+                splitted dataset or indexes
+
+        .. admonition:: Example
+            >>> from chainer_chemistry.datasets import NumpyTupleDataset
+            >>> from chainer_chemistry.dataset.splitters \
+            >>>                                 import StratifiedSplitter
+            >>> a = numpy.random.random((10, 10))
+            >>> b = numpy.random.random((10, 8))
+            >>> c = numpy.random.random((10, 1))
+            >>> d = NumpyTupleDataset(a, b, c)
+            >>> splitter = StratifiedSplitter()
+            >>> splitter.train_valid_test_split()
+            >>> train, valid, test =
+                splitter.train_valid_test_split(dataset, return_index=False)
+            >>> print(len(train), len(valid))
+            8, 1, 1
+        """
+
+        return super(StratifiedSplitter, self)\
+            .train_valid_test_split(dataset, frac_train, frac_valid, frac_test,
+                                    converter, return_index, seed=seed,
+                                    label_feature_id=labels_feature_id,
+                                    task_id=task_id)
+
+    def train_valid_split(self, dataset, labels_feature_id=-1, task_id=0,
+                          frac_train=0.9, frac_valid=0.1, converter=None,
+                          return_index=True, seed=None):
+        """Generate indices by stratified splittting dataset into train and
+        valid set.
+
+        Args:
+            dataset(NumpyTupleDataset, numpy.ndarray):
+                Dataset.
+            labels_feature_id(int):
+                Dataset feature ID in NumpyTupleDataset.
+            task_id(int):
+                Target task number for stratification.
+            seed (int):
+                Random seed.
+            frac_train(float):
+                Fraction of dataset put into training data.
+            frac_valid(float):
+                Fraction of dataset put into validation data.
+            converter(callable):
+            return_index(bool):
+                If `True`, this function returns only indexes. If `False`, this
+                function returns splitted dataset.
+
+        Returns:
+            SplittedDataset(tuple):
+                splitted dataset or indexes
+
+        .. admonition:: Example
+            >>> from chainer_chemistry.datasets import NumpyTupleDataset
+            >>> from chainer_chemistry.dataset.splitters \
+            >>>                                 import StratifiedSplitter
+            >>> a = numpy.random.random((10, 10))
+            >>> b = numpy.random.random((10, 8))
+            >>> c = numpy.random.random((10, 1))
+            >>> d = NumpyTupleDataset(a, b, c)
+            >>> splitter = StratifiedSplitter()
+            >>> splitter.train_valid_split()
+            >>> train, valid =
+                    splitter.train_valid_split(dataset, return_index=False)
+            >>> print(len(train), len(valid))
+            9, 1
+        """
+
+        return super(StratifiedSplitter, self)\
+            .train_valid_split(dataset, frac_train, frac_valid, converter,
+                               return_index, seed=seed, task_id=task_id,
+                               label_feature_id=labels_feature_id)
