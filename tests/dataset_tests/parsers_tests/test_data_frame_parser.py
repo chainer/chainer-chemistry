7cee7813720a3852a2a6c3d6ee72a7b92213ceee
corochann <acc1ssnn9terias@gmail.com>
2018-06-30 14:05:35 +0900

    test: add test for return_success_flag

--- tests/dataset_tests/parsers_tests/test_data_frame_parser.py
+++ tests/dataset_tests/parsers_tests/test_data_frame_parser.py
@@ -51,16 +51,19 @@ def check_features(actual, expect_input_features, expect_label):
 
 
 def test_data_frame_parser_not_return_smiles(data_frame, mols):
+    """Test default behavior"""
     preprocessor = NFPPreprocessor()
     parser = DataFrameParser(preprocessor, smiles_col='smiles')
     # Actually, `dataset, smiles = parser.parse(..)` is enough.
     result = parser.parse(data_frame, return_smiles=False)
     dataset = result['dataset']
     smiles = result['smiles']
+    success_flag = result['success_flag']
     assert len(dataset) == 3
     assert smiles is None
+    assert success_flag is None
 
-    # As we want test CSVFileParser, we assume
+    # As we want test DataFrameParser, we assume
     # NFPPreprocessor works as documented.
     for i in range(3):
         expect = preprocessor.get_input_features(mols[i])
@@ -77,8 +80,7 @@ def test_data_frame_parser_return_smiles(data_frame, mols, label_a):
     smiles = result['smiles']
     assert len(dataset) == 3
 
-    # As we want test CSVFileParser, we assume
-    # NFPPreprocessor works as documented.
+    # We assume NFPPreprocessor works as documented.
     for i in range(3):
         expect = preprocessor.get_input_features(mols[i])
         check_features(dataset[i], expect, label_a[i])
@@ -97,13 +99,16 @@ def test_data_frame_parser_target_index(data_frame, mols, label_a):
     preprocessor = NFPPreprocessor()
     parser = DataFrameParser(preprocessor, labels='labelA',
                              smiles_col='smiles')
-    result = parser.parse(data_frame, return_smiles=True, target_index=[0, 2])
+    result = parser.parse(data_frame, return_smiles=True, target_index=[0, 2],
+                          return_success_flag=True)
     dataset = result['dataset']
     smiles = result['smiles']
     assert len(dataset) == 2
+    success_flag = result['success_flag']
+    assert numpy.alltrue(success_flag)
+    assert len(success_flag) == 2
 
-    # As we want test CSVFileParser, we assume
-    # NFPPreprocessor works as documented.
+    # We assume NFPPreprocessor works as documented.
     expect = preprocessor.get_input_features(mols[0])
     check_features(dataset[0], expect, label_a[0])
 
@@ -116,6 +121,32 @@ def test_data_frame_parser_target_index(data_frame, mols, label_a):
     assert len(smiles) == len(dataset)
     assert smiles[0] == 'CN=C=O'
     assert smiles[1] == 'CC1=CC2CC(CC1)O2'
+
+
+def test_data_frame_parser_return_success_flag(mols, label_a):
+    """test `labels` option and retain_smiles=True."""
+    preprocessor = NFPPreprocessor()
+    parser = DataFrameParser(preprocessor, labels='labelA',
+                             smiles_col='smiles')
+    df = pandas.DataFrame({
+        'smiles': ['var', 'CN=C=O', 'hoge', 'Cc1ccccc1', 'CC1=CC2CC(CC1)O2'],
+        'labelA': [0., 2.1, 0., 5.3, -1.2],
+    })
+    result = parser.parse(df, return_smiles=True, return_success_flag=True)
+
+    dataset = result['dataset']
+    # smiles = result['smiles']
+    assert len(dataset) == 3
+    success_flag = result['success_flag']
+    assert len(success_flag) == 5
+    # print('success_flag', success_flag)
+    assert numpy.alltrue(success_flag[[1, 3, 4]])
+    assert numpy.alltrue(~success_flag[[0, 2]])
+
+    # We assume NFPPreprocessor works as documented.
+    for i in range(3):
+        expect = preprocessor.get_input_features(mols[i])
+        check_features(dataset[i], expect, label_a[i])
 
 
 if __name__ == '__main__':
