57a870def94f95361e05c94cbfaca77c841abe8b
Mihai Morariu <mihaimorariu@gmail.com>
2018-07-13 17:09:42 +0900

    Keep consistency with naming conventions in other examples.

--- examples/custom_dataset/test_custom_dataset.sh
+++ examples/custom_dataset/test_custom_dataset.sh
@@ -7,5 +7,13 @@ gpu=${1:--1}
 
 for method in nfp ggnn schnet weavenet rsgcn
 do
-    python train.py --datafile dataset.csv --method ${method} --label value1 --conv-layers 1 --gpu ${gpu} --epoch 1 --unit-num 10 --batchsize 32
+    python train_custom_dataset.py \
+        --datafile dataset.csv \
+        --method ${method} \
+        --label value1 \
+        --conv-layers 1 \
+        --gpu ${gpu} \
+        --epoch 1 \
+        --unit-num 10 \
+        --batchsize 32
 done
