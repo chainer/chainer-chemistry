1b69cdfd094812230c3796c02069847ef59c7060
corochann <acc1ssnn9terias@gmail.com>
2018-07-18 09:58:12 +0900

    update docstring

--- chainer_chemistry/utils/extend.py
+++ chainer_chemistry/utils/extend.py
@@ -1,4 +1,5 @@
 from collections import Iterable
+from logging import getLogger
 
 import numpy
 import six
@@ -14,27 +15,61 @@ def _to_list(a):
 
 
 def extend_node(node, out_size, axis=-1, value=0):
+    """Extend size of `node` array
+
+    For now, this function works same with `extend_array` method,
+    this is just an alias function.
+
+    Args:
+        node (numpy.ndarray): the array whose `axis` to be extended.
+            first axis is considered as "batch" axis.
+        out_size (int): target output size for specified `axis`.
+        axis (int): node feature axis to be extended.
+            Default is `axis=-1`, which extends only last axis.
+        value (int or float): value to be filled for extended place.
+
+    Returns (numpy.ndarray): extended `node` array, extended place is filled
+        with `value`
+
+    """
     return extend_arrays_to_size(
         node, out_size=out_size, axis=axis, value=value)
 
 
 def extend_adj(adj, out_size, axis=None, value=0):
+    """Extend size of `adj` array
+
+    For now, this function only differs default `axis` value from
+    `extend_array` method, this is an alias function.
+
+    Args:
+        adj (numpy.ndarray): the array whose `axis` to be extended.
+            first axis is considered as "batch" axis.
+        out_size (int): target output size for specified `axis`.
+        axis (int or None): node feature axis to be extended. Default is None,
+            in this case `axis=[-1, -2]` is used to extend last 2 axes.
+        value (int or float): value to be filled for extended place.
+
+    Returns (numpy.ndarray): extended `adj` array, extended place is filled
+        with `value`
+
+    """
     axis = axis or [-1, -2]
     return extend_arrays_to_size(
         adj, out_size=out_size, axis=axis, value=value)
 
 
 def extend_arrays_to_size(arrays, out_size, axis=-1, value=0):
-    """Extend size of `node` array
+    """Extend size of `arrays` array
 
     Args:
         arrays (numpy.ndarray): the array whose `axis` to be extended.
             first axis is considered as "batch" axis.
         out_size (int): target output size for specified `axis`.
-        axis (int): node feature axis to be extended.
+        axis (int or list): node feature axis to be extended.
         value (int or float): value to be filled for extended place.
 
-    Returns (numpy.ndarray): extended `node` array, extended place is filled
+    Returns (numpy.ndarray): extended array, extended place is filled
         with `value`
 
     """
@@ -45,6 +80,10 @@ def extend_arrays_to_size(arrays, out_size, axis=-1, value=0):
 
     axis = _to_list(axis)
     for ax in axis:
+        if ax == 0:
+            logger = getLogger(__name__)
+            logger.warning('axis 0 detected, but axis=0 is expected to be '
+                           'batch size dimension.')
         if out_shape[ax] > out_size:
             raise ValueError(
                 'current size={} is larger than out_size={} at axis={}'
@@ -54,8 +93,8 @@ def extend_arrays_to_size(arrays, out_size, axis=-1, value=0):
 
 
 def extend_arrays_to_shape(arrays, out_shape, value=0):
-    # TODO: update ref
     # Ref: `_concat_arrays_with_padding` method in chainer convert.py
+    # https://github.com/chainer/chainer/blob/master/chainer/dataset/convert.py
     xp = cuda.get_array_module(arrays[0])
     with cuda.get_device_from_array(arrays[0]):
         result = xp.full(out_shape, value, dtype=arrays[0].dtype)
