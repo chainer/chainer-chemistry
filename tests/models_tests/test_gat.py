c092a96ab18732806e516931f21a11cc26a386ac
mottodora <mottodora@gmail.com>
2018-08-16 11:34:34 +0900

    add gpu tests

--- tests/models_tests/test_gat.py
+++ tests/models_tests/test_gat.py
@@ -5,8 +5,8 @@ import pytest
 
 from chainer_chemistry.config import MAX_ATOMIC_NUM
 from chainer_chemistry.models.gat import GraphAttentionNetworks
-# from chainer_chemistry.utils.permutation import permute_adj
-# from chainer_chemistry.utils.permutation import permute_node
+from chainer_chemistry.utils.permutation import permute_adj
+from chainer_chemistry.utils.permutation import permute_node
 
 atom_size = 5
 out_dim = 4
@@ -42,14 +42,14 @@ def test_forward_cpu(model, data):
     check_forward(model, atom_data, adj_data)
 
 
-# @pytest.mark.gpu
-# def test_forward_gpu(model, data):
-#     atom_data, adj_data = cuda.to_gpu(data[0]), cuda.to_gpu(data[1])
-#     model.to_gpu()
-#     check_forward(model, atom_data, adj_data)
+@pytest.mark.gpu
+def test_forward_gpu(model, data):
+    atom_data, adj_data = cuda.to_gpu(data[0]), cuda.to_gpu(data[1])
+    model.to_gpu()
+    check_forward(model, atom_data, adj_data)
 
 
-# TODO(nakago): check why tolerance is high
+# TODO(mottodora): check why tolerance is high
 def test_backward_cpu(model, data):
     atom_data, adj_data, y_grad = data
     params = tuple(model.params())
@@ -58,25 +58,27 @@ def test_backward_cpu(model, data):
                                   atol=1e3, rtol=1e3)
 
 
-# # TODO(nakago): check why tolerance is high
-# @pytest.mark.gpu
-# def test_backward_gpu(model, data):
-#     atom_data, adj_data, y_grad = [cuda.to_gpu(d) for d in data]
-#     model.to_gpu()
-#     gradient_check.check_backward(model, (atom_data, adj_data), y_grad,
-#                                   atol=1e0, rtol=1e0)
-#
-#
-# def test_forward_cpu_graph_invariant(model, data):
-#     atom_data, adj_data = data[0], data[1]
-#     y_actual = cuda.to_cpu(model(atom_data, adj_data).data)
-#
-#     permutation_index = numpy.random.permutation(atom_size)
-#     permute_atom_data = permute_node(atom_data, permutation_index)
-#     permute_adj_data = permute_adj(adj_data, permutation_index)
-#     permute_y_actual = cuda.to_cpu(model(
-#         permute_atom_data, permute_adj_data).data)
-#     assert numpy.allclose(y_actual, permute_y_actual, rtol=1e-5, atol=1e-6)
+# TODO(nakago): check why tolerance is high
+@pytest.mark.gpu
+def test_backward_gpu(model, data):
+    atom_data, adj_data, y_grad = [cuda.to_gpu(d) for d in data]
+    model.to_gpu()
+    params = tuple(model.params())
+    gradient_check.check_backward(model, (atom_data, adj_data), y_grad,
+                                  params=params, no_grads=[True, True],
+                                  atol=1e3, rtol=1e3)
+
+
+def test_forward_cpu_graph_invariant(model, data):
+    atom_data, adj_data = data[0], data[1]
+    y_actual = cuda.to_cpu(model(atom_data, adj_data).data)
+
+    permutation_index = numpy.random.permutation(atom_size)
+    permute_atom_data = permute_node(atom_data, permutation_index)
+    permute_adj_data = permute_adj(adj_data, permutation_index)
+    permute_y_actual = cuda.to_cpu(model(
+        permute_atom_data, permute_adj_data).data)
+    assert numpy.allclose(y_actual, permute_y_actual, rtol=1e-5, atol=1e-6)
 
 
 if __name__ == '__main__':
