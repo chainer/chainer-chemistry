eb2254da0cb171dda7053228a772c0b6b0416ebe
natsukium <tm.tomatoes@gmail.com>
2018-11-30 19:11:15 +0900

    Update `models`

--- chainer_chemistry/models/ggnn.py
+++ chainer_chemistry/models/ggnn.py
@@ -1,12 +1,11 @@
 import chainer
 from chainer import cuda
 from chainer import functions
-from chainer import links
 
-import chainer_chemistry
 from chainer_chemistry.config import MAX_ATOMIC_NUM
 from chainer_chemistry.links import EmbedAtomID
-from chainer_chemistry.links import GraphLinear
+from chainer_chemistry.links import GGNNReadout
+from chainer_chemistry.links import GGNNUpdate
 
 
 class GGNN(chainer.Chain):
@@ -38,72 +37,18 @@ class GGNN(chainer.Chain):
         with self.init_scope():
             # Update
             self.embed = EmbedAtomID(out_size=hidden_dim, in_size=n_atom_types)
-            self.message_layers = chainer.ChainList(
-                *[GraphLinear(hidden_dim, self.NUM_EDGE_TYPE * hidden_dim)
-                  for _ in range(n_message_layer)]
-            )
-            self.update_layer = links.GRU(2 * hidden_dim, hidden_dim)
+            self.update_layer = GGNNUpdate(
+                hidden_dim=hidden_dim, n_layers=n_message_layer,
+                n_atom_types=self.NUM_EDGE_TYPE, weight_tying=weight_tying)
             # Readout
-            self.i_layers = chainer.ChainList(
-                *[GraphLinear(2 * hidden_dim, out_dim)
-                    for _ in range(n_readout_layer)]
-            )
-            self.j_layers = chainer.ChainList(
-                *[GraphLinear(hidden_dim, out_dim)
-                    for _ in range(n_readout_layer)]
-            )
+            self.readout_layer = GGNNReadout(
+                out_dim=out_dim, hidden_dim=hidden_dim,
+                n_layers=n_readout_layer, concat_hidden=concat_hidden)
         self.out_dim = out_dim
         self.hidden_dim = hidden_dim
         self.n_layers = n_layers
         self.concat_hidden = concat_hidden
         self.weight_tying = weight_tying
-
-    def update(self, h, adj, step=0):
-        # --- Message & Update part ---
-        # (minibatch, atom, ch)
-        mb, atom, ch = h.shape
-        out_ch = ch
-        message_layer_index = 0 if self.weight_tying else step
-        m = functions.reshape(self.message_layers[message_layer_index](h),
-                              (mb, atom, out_ch, self.NUM_EDGE_TYPE))
-        # m: (minibatch, atom, ch, edge_type)
-        # Transpose
-        m = functions.transpose(m, (0, 3, 1, 2))
-        # m: (minibatch, edge_type, atom, ch)
-
-        adj = functions.reshape(adj, (mb * self.NUM_EDGE_TYPE, atom, atom))
-        # (minibatch * edge_type, atom, out_ch)
-        m = functions.reshape(m, (mb * self.NUM_EDGE_TYPE, atom, out_ch))
-
-        m = chainer_chemistry.functions.matmul(adj, m)
-
-        # (minibatch * edge_type, atom, out_ch)
-        m = functions.reshape(m, (mb, self.NUM_EDGE_TYPE, atom, out_ch))
-        # Take sum
-        m = functions.sum(m, axis=1)
-        # (minibatch, atom, out_ch)
-
-        # --- Update part ---
-        # Contraction
-        h = functions.reshape(h, (mb * atom, ch))
-
-        # Contraction
-        m = functions.reshape(m, (mb * atom, ch))
-
-        out_h = self.update_layer(functions.concat((h, m), axis=1))
-        # Expansion
-        out_h = functions.reshape(out_h, (mb, atom, ch))
-        return out_h
-
-    def readout(self, h, h0, step=0):
-        # --- Readout part ---
-        index = step if self.concat_hidden else 0
-        # h, h0: (minibatch, atom, ch)
-        g = functions.sigmoid(
-            self.i_layers[index](functions.concat((h, h0), axis=2))) \
-            * self.j_layers[index](h)
-        g = functions.sum(g, axis=1)  # sum along atom's axis
-        return g
 
     def __call__(self, atom_array, adj):
         """Forward propagation
@@ -128,13 +73,13 @@ class GGNN(chainer.Chain):
         h0 = functions.copy(h, cuda.get_device_from_array(h.data).id)
         g_list = []
         for step in range(self.n_layers):
-            h = self.update(h, adj, step)
+            h = self.update_layer(h, adj, step)
             if self.concat_hidden:
-                g = self.readout(h, h0, step)
+                g = self.readout_layer(h, h0, step)
                 g_list.append(g)
 
         if self.concat_hidden:
             return functions.concat(g_list, axis=1)
         else:
-            g = self.readout(h, h0, 0)
+            g = self.readout_layer(h, h0, 0)
             return g
--- chainer_chemistry/models/relgcn.py
+++ chainer_chemistry/models/relgcn.py
@@ -1,10 +1,12 @@
 import chainer
-from chainer import cuda, functions
+from chainer import cuda
+from chainer import functions
 
-import chainer_chemistry
 from chainer_chemistry.config import MAX_ATOMIC_NUM
 from chainer_chemistry.links import EmbedAtomID
-from chainer_chemistry.links.graph_linear import GraphLinear
+from chainer_chemistry.links import GGNNReadout
+from chainer_chemistry.links import GraphLinear
+from chainer_chemistry.links import RelGCNUpdate
 
 
 def rescale_adj(adj):
@@ -28,105 +30,6 @@ def rescale_adj(adj):
     num_neighbors_inv = 1 / functions.where(cond, num_neighbors, base)
     return adj * functions.broadcast_to(
         num_neighbors_inv[:, None, None, :], adj.shape)
-
-
-class RelGCNUpdate(chainer.Chain):
-    """RelGUN submodule for update part
-
-    Args:
-        in_channels (int): input channel dimension
-        out_channels (int): output channel dimension
-        num_edge_type (int): number of types of edge
-    """
-
-    def __init__(self, in_channels, out_channels, num_edge_type=4):
-        super(RelGCNUpdate, self).__init__()
-        with self.init_scope():
-            self.graph_linear_self = GraphLinear(in_channels, out_channels)
-            self.graph_linear_edge = GraphLinear(
-                in_channels, out_channels * num_edge_type)
-        self.num_edge_type = num_edge_type
-        self.in_ch = in_channels
-        self.out_ch = out_channels
-
-    def __call__(self, h, adj):
-        """
-
-        Args:
-            h: (batchsize, num_nodes, in_channels)
-            adj: (batchsize, num_edge_type, num_nodes, num_nodes)
-
-        Returns:
-            (batchsize, num_nodes, ch)
-
-        """
-
-        mb, node, ch = h.shape
-
-        # --- self connection, apply linear function ---
-        hs = self.graph_linear_self(h)
-        # --- relational feature, from neighbor connection ---
-        # Expected number of neighbors of a vertex
-        # Since you have to divide by it, if its 0, you need to
-        # arbitrarily set it to 1
-        m = self.graph_linear_edge(h)
-        m = functions.reshape(m, (mb, node, self.out_ch, self.num_edge_type))
-        m = functions.transpose(m, (0, 3, 1, 2))
-        # m: (batchsize, edge_type, node, ch)
-        # hr: (batchsize, edge_type, node, ch)
-        hr = functions.matmul(adj, m)
-        # hr: (batchsize, node, ch)
-        hr = functions.sum(hr, axis=1)
-        return hs + hr
-
-
-class RelGCNReadout(chainer.Chain):
-    """RelGCN submodule for readout part
-
-    Args:
-        in_channels (int): dimension of feature vector associated to
-            each atom (node)
-        out_channels (int): output dimension of feature vector
-            associated to each molecule (graph)
-        nobias (bool): If ``True``, then this function does not use
-            the bias.
-    """
-
-    def __init__(self, in_channels, out_channels, nobias=True):
-        super(RelGCNReadout, self).__init__()
-        with self.init_scope():
-            self.sig_linear = chainer_chemistry.links.GraphLinear(
-                in_channels, out_channels, nobias=nobias)
-            self.tanh_linear = chainer_chemistry.links.GraphLinear(
-                in_channels, out_channels, nobias=nobias)
-
-    def __call__(self, h, x=None):
-        """
-        (implicit:
-            N is number of edges, R is number of types of relations)
-        Args:
-            h: (batchsize, num_nodes, ch)
-                N x F : Matrix of edges, each row is a molecule and
-                each column is a feature.
-                F_l is the number of features at layer l
-                F_0, the input layer, feature is type of molecule.
-                Softmaxed
-
-            x: (batchsize, num_nodes, ch)
-
-        Returns:
-            (batchsize, ch)
-            F_n : Graph level representation
-
-        """
-        if x is None:
-            in_feat = h
-        else:
-            in_feat = functions.concat([h, x], axis=2)
-        sig_feat = functions.sigmoid(self.sig_linear(in_feat))
-        tanh_feat = functions.tanh(self.tanh_linear(in_feat))
-
-        return functions.tanh(functions.sum(sig_feat * tanh_feat, axis=1))
 
 
 class RelGCN(chainer.Chain):
@@ -167,7 +70,9 @@ class RelGCN(chainer.Chain):
             self.rgcn_convs = chainer.ChainList(*[
                 RelGCNUpdate(ch_list[i], ch_list[i+1], num_edge_type)
                 for i in range(len(ch_list)-1)])
-            self.rgcn_readout = RelGCNReadout(ch_list[-1], out_channels)
+            self.rgcn_readout = GGNNReadout(
+                out_dim=out_channels, hidden_dim=ch_list[-1],
+                nobias=True, activate=functions.tanh)
         # self.num_relations = num_edge_type
         self.input_type = input_type
         self.scale_adj = scale_adj
