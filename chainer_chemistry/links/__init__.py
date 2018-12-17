d65e9e2c9fc5ef2c99e2af692db8b0115f3ed7a0
natsukium <tm.tomatoes@gmail.com>
2018-12-17 15:52:34 +0900

    mv update & readout to links/

--- chainer_chemistry/links/__init__.py
+++ chainer_chemistry/links/__init__.py
@@ -1,15 +1,15 @@
 from chainer_chemistry.links.connection.embed_atom_id import EmbedAtomID  # NOQA
 from chainer_chemistry.links.connection.graph_linear import GraphLinear  # NOQA
 
-from chainer_chemistry.links.connection.readout.ggnn_readout import GGNNReadout  # NOQA
-from chainer_chemistry.links.connection.readout.nfp_readout import NFPReadout  # NOQA
-from chainer_chemistry.links.connection.readout.rsgcn_readout import RSGCNReadout  # NOQA
-from chainer_chemistry.links.connection.readout.schnet_readout import SchNetReadout  # NOQA
-
-from chainer_chemistry.links.connection.update.ggnn_update import GGNNUpdate  # NOQA
-from chainer_chemistry.links.connection.update.nfp_update import NFPUpdate  # NOQA
-from chainer_chemistry.links.connection.update.relgcn_update import RelGCNUpdate  # NOQA
-from chainer_chemistry.links.connection.update.rsgcn_update import RSGCNUpdate  # NOQA
-from chainer_chemistry.links.connection.update.schnet_update import SchNetUpdate  # NOQA
-
 from chainer_chemistry.links.normalization.graph_batch_normalization import GraphBatchNormalization  # NOQA
+
+from chainer_chemistry.links.readout.ggnn_readout import GGNNReadout  # NOQA
+from chainer_chemistry.links.readout.nfp_readout import NFPReadout  # NOQA
+from chainer_chemistry.links.readout.rsgcn_readout import RSGCNReadout  # NOQA
+from chainer_chemistry.links.readout.schnet_readout import SchNetReadout  # NOQA
+
+from chainer_chemistry.links.update.ggnn_update import GGNNUpdate  # NOQA
+from chainer_chemistry.links.update.nfp_update import NFPUpdate  # NOQA
+from chainer_chemistry.links.update.relgcn_update import RelGCNUpdate  # NOQA
+from chainer_chemistry.links.update.rsgcn_update import RSGCNUpdate  # NOQA
+from chainer_chemistry.links.update.schnet_update import SchNetUpdate  # NOQA
