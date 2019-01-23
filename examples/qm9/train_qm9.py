98f0d0f175286bac722fdae80bf87a6c0bf24170
corochann <acc1ssnn9terias@gmail.com>
2019-01-23 10:43:23 +0900

    fix for case when cache_dir exists

--- examples/qm9/train_qm9.py
+++ examples/qm9/train_qm9.py
@@ -248,7 +248,7 @@ def main():
             dataset = D.get_qm9(preprocessor, labels=labels)
 
         # Cache the laded dataset.
-        os.makedirs(cache_dir)
+        os.makedirs(cache_dir, exist_ok=True)
         NumpyTupleDataset.save(dataset_cache_path, dataset)
 
     # Scale the label values, if necessary.
