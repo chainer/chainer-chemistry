fe4f8636a3128086d3300c3fa1a0ca50aa58c52e
corochann <acc1ssnn9terias@gmail.com>
2019-01-24 18:45:38 +0900

    fix default label argument

--- examples/qm9/train_qm9.py
+++ examples/qm9/train_qm9.py
@@ -97,7 +97,7 @@ def parse_arguments():
     parser.add_argument('--method', '-m', type=str, choices=method_list,
                         default='nfp', help='method name')
     parser.add_argument('--label', '-l', type=str,
-                        choices=label_names + ['all'], default='',
+                        choices=label_names + ['all'], default='all',
                         help='target label for regression; all means '
                         'predicting all properties at once')
     parser.add_argument('--scale', type=str, choices=scale_list,
