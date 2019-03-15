80dad51ce270c73e886055d8c1493fa8917455f2
mottodora <mottodora@gmail.com>
2019-03-15 23:25:45 +0900

    Add transmitter and WarpGate unit

--- chainer_chemistry/models/gwm.py
+++ chainer_chemistry/models/gwm.py
@@ -5,128 +5,95 @@ from chainer import links
 from chainer_chemistry.links import GraphLinear
 
 
-class GWM(chainer.Chain):
-    """
-    Graph Warping Module (GWM)
-
-    See: Ishiguro, Maeda, and Koyama. "Graph Warp Module: an Auxiliary Module
-        for Boosting the Power of Graph NeuralNetworks", arXiv, 2019.
-
-    Args:
-        hidden_dim (default=16): dimension of hidden vectors
-            associated to each atom (local node)
-        hidden_dim_super(default=16); dimension of super-node hidden vector
-        n_layers (default=4): number of layers
-        n_heads (default=8): numbef of heads
-        n_atom_types (default=MAX_ATOMIC_NUM): number of types of atoms
-        n_super_feature (default: tuned according to gtn_preprocessor):
-            number of super-node observation attributes
-        n_edge_types (int): number of edge types witin graphs.
-        dropout_ratio (default=0.5); if > 0.0, perform dropout
-        tying_flag (default=false): enable if you want to share params across
-            layers
-    """
-    NUM_EDGE_TYPE = 4
-
-    def __init__(self, hidden_dim=16, hidden_dim_super=16, n_layers=4,
-                 n_heads=8, dropout_ratio=0.5, concat_hidden=False,
-                 tying_flag=False):
-        super(GWM, self).__init__()
-        num_layer = n_layers
-        if tying_flag:
-            num_layer = 1
+class WarpGateUnit(chainer.Chain):
+    def __init__(self, output_type='graph', hidden_dim=16, n_layers=4,
+                 dropout_ratio=-1):
+        super(WarpGateUnit, self).__init__()
+        if output_type == 'graph':
+            LinearFunc = GraphLinear
+        elif output_type == 'super':
+            LinearFunc = links.Linear
+        else:
+            raise ValueError
 
         with self.init_scope():
-            self.update_super = chainer.ChainList(
-                *[links.Linear(in_size=hidden_dim_super,
-                               out_size=hidden_dim_super)
-                  for _ in range(num_layer)]
+            self.H = chainer.ChainList(
+                *[LinearFunc(in_size=hidden_dim, out_size=hidden_dim)
+                  for _ in range(n_layers)]
             )
-            #
-            # for Transmitter unit
-            #
+            self.G = chainer.ChainList(
+                *[LinearFunc(in_size=hidden_dim, out_size=hidden_dim)
+                  for _ in range(n_layers)]
+            )
+
+        self.hidden_dim = hidden_dim
+        self.n_layers = n_layers
+        self.dropout_ratio = dropout_ratio
+        self.output_type = output_type
+
+    def __call__(self, h, g, step=0):
+        z = self.H[step](h) + self.G[step](g)
+
+        if self.dropout_ratio > 0.0:
+            # TODO: fail backward test
+            z = functions.dropout(z, ratio=self.dropout_ratio)
+        z = functions.sigmoid(z)
+        merged = (1 - z) * h + z * g
+        return merged
+
+
+class SuperNodeTransmitterUnit(chainer.Chain):
+    def __init__(self, hidden_dim_super=16, hidden_dim=16, n_layers=4, dropout_ratio=-1):
+        super(SuperNodeTransmitterUnit, self).__init__()
+        with self.init_scope():
             self.F_super = chainer.ChainList(
                 *[links.Linear(in_size=hidden_dim_super,
                                out_size=hidden_dim)
-                  for _ in range(num_layer)]
+                  for _ in range(n_layers)]
             )
-            self.V_super = chainer.ChainList(
-                *[links.Linear(hidden_dim * n_heads, hidden_dim * n_heads)
-                  for _ in range(num_layer)]
-            )
-            self.W_super = chainer.ChainList(
-                *[links.Linear(hidden_dim * n_heads, hidden_dim_super)
-                  for _ in range(num_layer)]
-            )
-            self.B = chainer.ChainList(
-                *[GraphLinear(n_heads * hidden_dim, n_heads * hidden_dim_super)
-                  for _ in range(num_layer)]
-            )
-
-            #
-            # for Warp Gate unit
-            #
-            self.gate_dim = hidden_dim
-            self.H_local = chainer.ChainList(
-                *[GraphLinear(in_size=hidden_dim, out_size=self.gate_dim)
-                  for _ in range(num_layer)]
-            )
-            self.G_local = chainer.ChainList(
-                *[GraphLinear(in_size=hidden_dim, out_size=self.gate_dim)
-                  for _ in range(num_layer)]
-            )
-
-            self.gate_dim_super = hidden_dim_super
-            self.H_super = chainer.ChainList(
-                *[links.Linear(in_size=hidden_dim_super,
-                               out_size=self.gate_dim_super)
-                  for _ in range(num_layer)]
-            )
-            self.G_super = chainer.ChainList(
-                *[links.Linear(in_size=hidden_dim_super,
-                               out_size=self.gate_dim_super)
-                  for _ in range(num_layer)]
-            )
-
-            # GRU's. not layer-wise (recurrent through layers)
-            self.GRU_local = links.GRU(in_size=hidden_dim, out_size=hidden_dim)
-            self.GRU_super = links.GRU(in_size=hidden_dim_super,
-                                       out_size=hidden_dim_super)
-        # end init_scope-with
         self.hidden_dim = hidden_dim
         self.hidden_dim_super = hidden_dim_super
         self.n_layers = n_layers
-        self.n_heads = n_heads
         self.dropout_ratio = dropout_ratio
-        self.concat_hidden = concat_hidden
-        self.tying_flag = tying_flag
 
-    def __call__(self, h, h_new, g, step=0):
-        """
-        Describes the module for a single layer update.
-        Do not forget to rest GRU for each batch...
+    def __call__(self, g, n_atoms, step=0):
+        mb = len(g)
+        # for local updates
+        g_trans = self.F_super[step](g)
+        # intermediate_h_super.shape == (mb, self.hidden_dim)
+        g_trans = functions.tanh(g_trans)
+        # intermediate_h_super.shape == (mb, 1, self.hidden_dim)
+        g_trans = functions.expand_dims(g_trans, 1)
+        # intermediate_h_super.shape == (mb, atom, self.hidden_dim)
+        g_trans = functions.broadcast_to(g_trans, (mb, n_atoms, self.hidden_dim))
+        return g_trans
 
-        :param h: minibatch by num_nodes by hidden_dim numpy array.
-                current local node hidden states as input of the vanilla GNN
-        :param h_new: minibatch by num_nodes by hidden_dim numpy array.
-                updated local node hidden states as output from the vanilla GNN
-        :param adj: minibatch by bond_types by num_nodes by num_nodes 1/0
-                array. Adjacency matrices over several bond types
-        :param g: minibatch by hidden_dim_super numpy array.
-                current super node hiddden state
-        :param step: integer, the layer index
-        :return: updated h and g
-        """
-        # (minibatch, atom, ch)
+
+class GraphTransmitterUnit(chainer.Chain):
+    def __init__(self, hidden_dim_super=16, hidden_dim=16, n_heads=8,
+                 n_layers=4, dropout_ratio=-1):
+        super(GraphTransmitterUnit, self).__init__()
+        with self.init_scope():
+            self.V_super = chainer.ChainList(
+                *[links.Linear(hidden_dim * n_heads, hidden_dim * n_heads)
+                  for _ in range(n_layers)]
+            )
+            self.W_super = chainer.ChainList(
+                *[links.Linear(hidden_dim * n_heads, hidden_dim_super)
+                  for _ in range(n_layers)]
+            )
+            self.B = chainer.ChainList(
+                *[GraphLinear(n_heads * hidden_dim, n_heads * hidden_dim_super)
+                  for _ in range(n_layers)]
+            )
+        self.hidden_dim = hidden_dim
+        self.hidden_dim_super = hidden_dim_super
+        self.n_layers = n_layers
+        self.dropout_ratio = dropout_ratio
+        self.n_heads = n_heads
+
+    def __call__(self, h, g, step=0):
         mb, atom, ch = h.shape
-        #
-        # Transmitter unit: inter-module message passing
-        #
-        # non linear update of the super node
-        g_new = functions.relu(self.update_super[step](g))
-
-        # original --> super transmission
-
         # h1.shape == (mb, atom, 1, ch)
         h1 = functions.expand_dims(h, 2)
         # h1.shape == (mb, atom, n_heads, ch)
@@ -187,40 +154,106 @@ class GWM(chainer.Chain):
         h_trans = self.W_super[step](h_trans)
         # intermediate_h.shape == (mb, self.hidden_dim_super)
         h_trans = functions.tanh(h_trans)
+        return h_trans
 
+
+class GWM(chainer.Chain):
+    """
+    Graph Warping Module (GWM)
+
+    See: Ishiguro, Maeda, and Koyama. "Graph Warp Module: an Auxiliary Module
+        for Boosting the Power of Graph NeuralNetworks", arXiv, 2019.
+
+    Args:
+        hidden_dim (default=16): dimension of hidden vectors
+            associated to each atom (local node)
+        hidden_dim_super(default=16); dimension of super-node hidden vector
+        n_layers (default=4): number of layers
+        n_heads (default=8): numbef of heads
+        n_atom_types (default=MAX_ATOMIC_NUM): number of types of atoms
+        n_super_feature (default: tuned according to gtn_preprocessor):
+            number of super-node observation attributes
+        n_edge_types (int): number of edge types witin graphs.
+        dropout_ratio (default=0.5); if > 0.0, perform dropout
+        tying_flag (default=false): enable if you want to share params across
+            layers
+    """
+    NUM_EDGE_TYPE = 4
+
+    def __init__(self, hidden_dim=16, hidden_dim_super=16, n_layers=4,
+                 n_heads=8, dropout_ratio=-1, concat_hidden=False,
+                 tying_flag=False):
+        super(GWM, self).__init__()
+        num_layer = n_layers
+        if tying_flag:
+            num_layer = 1
+
+        with self.init_scope():
+            self.update_super = chainer.ChainList(
+                *[links.Linear(in_size=hidden_dim_super,
+                               out_size=hidden_dim_super)
+                  for _ in range(num_layer)]
+            )
+
+            # for Transmitter unit
+            self.super_transmitter = SuperNodeTransmitterUnit(
+                hidden_dim=hidden_dim, hidden_dim_super=hidden_dim_super,
+                n_layers=n_layers, dropout_ratio=dropout_ratio)
+            self.graph_transmitter = GraphTransmitterUnit(
+                hidden_dim=hidden_dim, hidden_dim_super=hidden_dim_super,
+                n_layers=n_layers, n_heads=n_heads, dropout_ratio=dropout_ratio)
+
+            # for Warp Gate unit
+            self.wgu_local = WarpGateUnit(output_type='graph', hidden_dim=hidden_dim,
+                                          n_layers=n_layers, dropout_ratio=dropout_ratio)
+            self.wgu_super = WarpGateUnit(output_type='super', hidden_dim=hidden_dim_super,
+                                          n_layers=n_layers, dropout_ratio=dropout_ratio)
+
+            # GRU's. not layer-wise (recurrent through layers)
+            self.GRU_local = links.GRU(in_size=hidden_dim, out_size=hidden_dim)
+            self.GRU_super = links.GRU(in_size=hidden_dim_super,
+                                       out_size=hidden_dim_super)
+        # end init_scope-with
+        self.hidden_dim = hidden_dim
+        self.hidden_dim_super = hidden_dim_super
+        self.n_layers = n_layers
+        self.n_heads = n_heads
+        self.dropout_ratio = dropout_ratio
+        self.concat_hidden = concat_hidden
+        self.tying_flag = tying_flag
+
+    def __call__(self, h, h_new, g, step=0):
+        """
+        Describes the module for a single layer update.
+        Do not forget to rest GRU for each batch...
+
+        :param h: minibatch by num_nodes by hidden_dim numpy array.
+                current local node hidden states as input of the vanilla GNN
+        :param h_new: minibatch by num_nodes by hidden_dim numpy array.
+                updated local node hidden states as output from the vanilla GNN
+        :param adj: minibatch by bond_types by num_nodes by num_nodes 1/0
+                array. Adjacency matrices over several bond types
+        :param g: minibatch by hidden_dim_super numpy array.
+                current super node hiddden state
+        :param step: integer, the layer index
+        :return: updated h and g
+        """
+        # (minibatch, atom, ch)
+        mb, atom, ch = h.shape
+        # non linear update of the super node
+        g_new = functions.relu(self.update_super[step](g))
+
+        # Transmitter unit: inter-module message passing
+        # original --> super transmission
+        h_trans = self.graph_transmitter(h, g, step=step)
         # g_trans: super --> original transmission
+        g_trans = self.super_transmitter(g, atom, step=step)
 
-        # for local updates
-        g_trans = self.F_super[step](g)
-        # intermediate_h_super.shape == (mb, self.hidden_dim)
-        g_trans = functions.tanh(g_trans)
-        # intermediate_h_super.shape == (mb, 1, self.hidden_dim)
-        g_trans = functions.expand_dims(g_trans, 1)
-        # intermediate_h_super.shape == (mb, atom, self.hidden_dim)
-        g_trans = functions.broadcast_to(g_trans, (mb, atom, self.hidden_dim))
-
-        #
         # Warp Gate unit
-        #
-        z_local = self.H_local[step](h_new) + self.G_local[step](g_trans)
-        z_local = functions.broadcast_to(z_local, (mb, atom, self.hidden_dim))
-        if self.dropout_ratio > 0.0:
-            z_local = functions.dropout(z_local, ratio=self.dropout_ratio)
-        z_local = functions.sigmoid(z_local)
-        #  new_h.shape == (mb, atom, ch)
-        merged_h = (1.0-z_local) * h_new + z_local * g_trans
+        merged_h = self.wgu_local(h_new, g_trans, step=step)
+        merged_g = self.wgu_super(h_trans, g_new, step=step)
 
-        z_super = self.H_super[step](h_trans) + self.G_super[step](g_new)
-        z_super = functions.broadcast_to(z_super, (mb, self.hidden_dim_super))
-        if self.dropout_ratio > 0.0:
-            z_super = functions.dropout(z_super, ratio=self.dropout_ratio)
-        z_super = functions.sigmoid(z_super)
-        merged_g = (1.0-z_super) * h_trans + z_super * g_new
-        # assert out_h_super.shape==(mb, self.hidden_dim_super)
-
-        #
         # Self recurrent
-        #
         out_h = functions.reshape(merged_h, (mb * atom, self.hidden_dim))
         out_h = self.GRU_local(out_h)
         out_h = functions.reshape(out_h, (mb, atom, self.hidden_dim))
--- tests/models_tests/test_gwm.py
+++ tests/models_tests/test_gwm.py
@@ -5,9 +5,8 @@ import numpy
 import pytest
 
 from chainer_chemistry.config import MAX_ATOMIC_NUM
-from chainer_chemistry.dataset.preprocessors.common import construct_supernode_feature  # NOQA
 from chainer_chemistry.links.connection.embed_atom_id import EmbedAtomID
-from chainer_chemistry.models.gwm import GWM
+from chainer_chemistry.models.gwm import GWM, WarpGateUnit, SuperNodeTransmitterUnit, GraphTransmitterUnit
 from chainer_chemistry.utils.permutation import permute_adj
 from chainer_chemistry.utils.permutation import permute_node
 
@@ -19,8 +18,36 @@ num_edge_type = 2
 
 
 @pytest.fixture
+def graph_warp_gate_unit():
+    return WarpGateUnit(output_type='graph', hidden_dim=hidden_dim,
+                        n_layers=1)
+
+
+@pytest.fixture
+def super_warp_gate_unit():
+    return WarpGateUnit(output_type='super', hidden_dim=supernode_dim,
+                        n_layers=1)
+
+@pytest.fixture
+def super_warp_gate_unit():
+    return WarpGateUnit(output_type='super', hidden_dim=supernode_dim,
+                        n_layers=1)
+
+@pytest.fixture
+def super_node_transmitter_unit():
+    return SuperNodeTransmitterUnit(hidden_dim_super=supernode_dim,
+                                    hidden_dim=hidden_dim, n_layers=1)
+
+@pytest.fixture
+def graph_transmitter_unit():
+    return GraphTransmitterUnit(hidden_dim_super=supernode_dim, hidden_dim=hidden_dim,
+                                n_layers=1)
+
+
+@pytest.fixture
 def gwm():
-    return GWM(hidden_dim=hidden_dim, hidden_dim_super=supernode_dim)
+    return GWM(hidden_dim=hidden_dim, hidden_dim_super=supernode_dim,
+               n_layers=1)
 
 
 @pytest.fixture
@@ -29,27 +56,92 @@ def data():
     atom_data = numpy.random.randint(
         0, high=MAX_ATOMIC_NUM, size=(batch_size, atom_size)
         ).astype('i')
+    new_atom_data = numpy.random.randint(
+        0, high=MAX_ATOMIC_NUM, size=(batch_size, atom_size)
+    ).astype('i')
     y_grad = numpy.random.uniform(
         -1, 1, (batch_size, atom_size, hidden_dim)).astype('f')
-    supernode = numpy.random.uniform(0, 1, (batch_size, supernode_dim))\
+    supernode = numpy.random.uniform(-1.0, 1.0, (batch_size, supernode_dim))\
         .astype('f')
+    supernode_grad = numpy.random.uniform(
+        -1, 1, (batch_size, supernode_dim)).astype('f')
 
     embed = EmbedAtomID(in_size=MAX_ATOMIC_NUM, out_size=hidden_dim)
     embed_atom_data = embed(atom_data).data
-    return embed_atom_data, supernode, y_grad
+    new_embed_atom_data = embed(new_atom_data).data
+    return embed_atom_data, new_embed_atom_data, supernode, y_grad,\
+           supernode_grad
 
 
-def check_forward(gwm, embed_atom_data, supernode):
+def test_graph_transmitter_unit_forward(graph_transmitter_unit, data):
+    embed_atom_data = data[0]
+    supernode = data[2]
+    h_trans = graph_transmitter_unit(embed_atom_data, supernode)
+    assert h_trans.array.shape == (batch_size, supernode_dim)
+
+
+def test_graph_transmitter_unit_backward(graph_transmitter_unit, data):
+    embed_atom_data = data[0]
+    supernode = data[2]
+    supernode_grad = data[4]
+    gradient_check.check_backward(graph_transmitter_unit, (embed_atom_data, supernode),
+                                  supernode_grad, eps=0.1, detect_nondifferentiable=True)
+
+
+def test_super_node_transmitter_unit_forward(super_node_transmitter_unit, data):
+    supernode = data[2]
+    g_trans = super_node_transmitter_unit(supernode, atom_size)
+    assert g_trans.array.shape == (batch_size, atom_size, hidden_dim)
+
+
+def test_super_node_transmitter_unit_backward(super_node_transmitter_unit, data):
+    supernode = data[2]
+    y_grad = data[3]
+    gradient_check.check_backward(lambda x: super_node_transmitter_unit(x, atom_size), supernode,
+                                  y_grad, detect_nondifferentiable=True)
+
+
+def test_graph_warp_gate_unit_forward(graph_warp_gate_unit, data):
+    embed_atom_data = data[0]
+    new_embed_atom_data = data[1]
+    merged = graph_warp_gate_unit(embed_atom_data, new_embed_atom_data)
+    assert merged.array.shape == (batch_size, atom_size, hidden_dim)
+
+
+def test_graph_warp_gate_unit_backward(graph_warp_gate_unit, data):
+    embed_atom_data = data[0]
+    new_embed_atom_data = data[1]
+    y_grad = data[3]
+    gradient_check.check_backward(graph_warp_gate_unit,
+                                  (embed_atom_data, new_embed_atom_data),
+                                  y_grad, eps=0.01, detect_nondifferentiable=True)
+
+
+def test_super_warp_gate_unit_forward(super_warp_gate_unit, data):
+    supernode = data[2]
+    merged = super_warp_gate_unit(supernode, supernode)
+    assert merged.array.shape == (batch_size, supernode_dim)
+
+
+def test_super_warp_gate_unit_backward(super_warp_gate_unit, data):
+    supernode = data[2]
+    supernode_grad = data[4]
+    gradient_check.check_backward(super_warp_gate_unit,
+                                  (supernode, supernode),
+                                  supernode_grad, eps=0.01, detect_nondifferentiable=True)
+
+
+def check_forward(gwm, embed_atom_data, new_embed_atom_data, supernode):
     gwm.GRU_local.reset_state()
     gwm.GRU_super.reset_state()
-    h_actual, g_actual = gwm(embed_atom_data, embed_atom_data, supernode)
+    h_actual, g_actual = gwm(embed_atom_data, new_embed_atom_data, supernode)
     assert h_actual.array.shape == (batch_size, atom_size, hidden_dim)
     assert g_actual.array.shape == (batch_size, supernode_dim)
 
 
 def test_forward_cpu(gwm, data):
-    embed_atom_data, supernode = data[:2]
-    check_forward(gwm, embed_atom_data, supernode)
+    embed_atom_data, new_embed_atom_data, supernode = data[:3]
+    check_forward(gwm, embed_atom_data, new_embed_atom_data, supernode)
 
 
 # @pytest.mark.gpu
@@ -59,37 +151,17 @@ def test_forward_cpu(gwm, data):
 #     check_forward(update, atom_data, adj_data)
 #
 #
-# def check_backward(update, atom_data, adj_data, y_grad):
-#     """Check gradient of GGNNUpdate.
-#
-#     This function is different from other backward tests.
-#     Because of GRU, reset_state method has to be called explicitly
-#     before gradient calculation.
-#
-#     Args:
-#         update (callable):
-#         atom_data (numpy.ndarray):
-#         adj_data (numpy.ndarray):
-#         y_grad (numpy.ndarray):
-#     """
-#     atom = chainer.Variable(atom_data)
-#     adj = chainer.Variable(adj_data)
-#     update.reset_state()
-#     y = update(atom, adj)
-#     y.grad = y_grad
-#     y.backward()
-#
-#     def f():
-#         update.reset_state()
-#         return update(atom_data, adj_data).data,
-#
-#     gx, = gradient_check.numerical_grad(f, (atom.data, ), (y.grad, ))
-#     numpy.testing.assert_allclose(
-#         cuda.to_cpu(gx), cuda.to_cpu(atom.grad), atol=1e-3, rtol=1e-3)
-#
-#
-# def test_backward_cpu(update, data):
-#     check_backward(update, *data)
+def check_backward(gwm, embed_atom_data, new_embed_atom_data, supernode,
+                   y_grad, supernode_grad):
+    gwm.GRU_local.reset_state()
+    gwm.GRU_super.reset_state()
+    gradient_check.check_backward(gwm, (embed_atom_data, new_embed_atom_data,
+                                        supernode), (y_grad, supernode_grad),
+                                  eps=0.1, detect_nondifferentiable=False)
+
+
+def test_backward_cpu(gwm, data):
+    check_backward(gwm, *data)
 #
 #
 # @pytest.mark.gpu
