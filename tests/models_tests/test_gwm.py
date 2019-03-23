80986ff286495806256f7362b02d7d5d1260d820
mottodora <mottodora@gmail.com>
2019-03-23 13:12:23 +0900

    fix for old chainer

--- tests/models_tests/test_gwm.py
+++ tests/models_tests/test_gwm.py
@@ -4,7 +4,7 @@ from chainer import gradient_check
 import numpy
 import pytest
 
-from chainer_chemistry.models.gwm import GWM, WarpGateUnit, SuperNodeTransmitterUnit, GraphTransmitterUnit
+from chainer_chemistry.models.gwm import GWM, WarpGateUnit, SuperNodeTransmitterUnit, GraphTransmitterUnit  # NOQA
 from chainer_chemistry.utils.permutation import permute_node
 
 atom_size = 5
@@ -32,7 +32,8 @@ def super_node_transmitter_unit():
 
 @pytest.fixture
 def graph_transmitter_unit():
-    return GraphTransmitterUnit(hidden_dim_super=supernode_dim, hidden_dim=hidden_dim)
+    return GraphTransmitterUnit(hidden_dim_super=supernode_dim,
+                                hidden_dim=hidden_dim)
 
 
 @pytest.fixture
@@ -60,7 +61,7 @@ def data():
         -0.01, 0.01, (batch_size, supernode_dim)).astype('f')
 
     return embed_atom_data, new_embed_atom_data, supernode, y_grad,\
-           supernode_grad
+        supernode_grad
 
 
 def test_graph_transmitter_unit_forward(graph_transmitter_unit, data):
@@ -74,21 +75,24 @@ def test_graph_transmitter_unit_backward(graph_transmitter_unit, data):
     embed_atom_data = data[0]
     supernode = data[2]
     supernode_grad = data[4]
-    gradient_check.check_backward(graph_transmitter_unit, (embed_atom_data, supernode),
-                                  supernode_grad, eps=0.1, detect_nondifferentiable=True)
+    gradient_check.check_backward(graph_transmitter_unit,
+                                  (embed_atom_data, supernode),
+                                  supernode_grad, eps=0.1)
 
 
-def test_super_node_transmitter_unit_forward(super_node_transmitter_unit, data):
+def test_super_node_transmitter_unit_forward(super_node_transmitter_unit,
+                                             data):
     supernode = data[2]
     g_trans = super_node_transmitter_unit(supernode, atom_size)
     assert g_trans.array.shape == (batch_size, atom_size, hidden_dim)
 
 
-def test_super_node_transmitter_unit_backward(super_node_transmitter_unit, data):
+def test_super_node_transmitter_unit_backward(super_node_transmitter_unit,
+                                              data):
     supernode = data[2]
     y_grad = data[3]
-    gradient_check.check_backward(lambda x: super_node_transmitter_unit(x, atom_size), supernode,
-                                  y_grad, detect_nondifferentiable=True)
+    gradient_check.check_backward(
+        lambda x: super_node_transmitter_unit(x, atom_size), supernode, y_grad)
 
 
 def test_graph_warp_gate_unit_forward(graph_warp_gate_unit, data):
@@ -104,7 +108,7 @@ def test_graph_warp_gate_unit_backward(graph_warp_gate_unit, data):
     y_grad = data[3]
     gradient_check.check_backward(graph_warp_gate_unit,
                                   (embed_atom_data, new_embed_atom_data),
-                                  y_grad, eps=0.01, detect_nondifferentiable=True)
+                                  y_grad, eps=0.01)
 
 
 def test_super_warp_gate_unit_forward(super_warp_gate_unit, data):
@@ -118,7 +122,7 @@ def test_super_warp_gate_unit_backward(super_warp_gate_unit, data):
     supernode_grad = data[4]
     gradient_check.check_backward(super_warp_gate_unit,
                                   (supernode, supernode),
-                                  supernode_grad, eps=0.01, detect_nondifferentiable=True)
+                                  supernode_grad, eps=0.01)
 
 
 def check_forward(gwm, embed_atom_data, new_embed_atom_data, supernode):
@@ -135,7 +139,7 @@ def test_forward_cpu(gwm, data):
 
 
 @pytest.mark.gpu
-def test_forward_cpu(gwm, data):
+def test_forward_gpu(gwm, data):
     embed_atom_data, new_embed_atom_data, supernode = data[:3]
     embed_atom_data = cuda.to_gpu(embed_atom_data)
     new_embed_atom_data = cuda.to_gpu(new_embed_atom_data)
@@ -153,8 +157,7 @@ def check_backward(gwm, embed_atom_data, new_embed_atom_data, supernode,
     # numerical differentiation
     gradient_check.check_backward(gwm, (embed_atom_data, new_embed_atom_data,
                                         supernode), (y_grad, supernode_grad),
-                                  eps=0.1, detect_nondifferentiable=False,
-                                  rtol=1e-0)
+                                  eps=0.1, rtol=1e-0)
 
 
 def test_backward_cpu(gwm, data):
@@ -173,16 +176,19 @@ def test_forward_cpu_graph_invariant(gwm, data):
     embed_atom_data, new_embed_atom_data, supernode = data[:3]
     h_actual, g_actual = gwm(embed_atom_data, new_embed_atom_data, supernode)
 
-    permute_embed_atom_data = permute_node(embed_atom_data, permutation_index, axis=1)
-    permute_new_embed_atom_data = permute_node(new_embed_atom_data, permutation_index, axis=1)
+    permute_embed_atom_data = permute_node(
+        embed_atom_data, permutation_index, axis=1)
+    permute_new_embed_atom_data = permute_node(
+        new_embed_atom_data, permutation_index, axis=1)
     gwm.reset_state()
-    permute_h_actual, permute_g_actual = gwm(permute_embed_atom_data, permute_new_embed_atom_data,
-                                             supernode)
+    permute_h_actual, permute_g_actual = gwm(
+        permute_embed_atom_data, permute_new_embed_atom_data, supernode)
     numpy.testing.assert_allclose(
         permute_node(h_actual.data, permutation_index, axis=1),
         permute_h_actual.data, rtol=1e-5, atol=1e-5)
 
-    numpy.testing.assert_allclose(g_actual.data, permute_g_actual.data, rtol=1e-5, atol=1e-5)
+    numpy.testing.assert_allclose(g_actual.data, permute_g_actual.data,
+                                  rtol=1e-5, atol=1e-5)
 
 
 if __name__ == '__main__':
