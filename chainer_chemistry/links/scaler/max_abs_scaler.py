350725de6ad0f1ad9b85681caf461142e5a67fd2
corochann <acc1ssnn9terias@gmail.com>
2019-04-29 08:12:40 +0900

    implement max abs scaler

--- chainer_chemistry/links/scaler/max_abs_scaler.py
+++ chainer_chemistry/links/scaler/max_abs_scaler.py
@@ -22,10 +22,8 @@ class MaxAbsScaler(BaseScaler):
         super(MaxAbsScaler, self).__init__()
         self.indices = None
         self.register_persistent('indices')
-        self.mean = None
-        self.register_persistent('mean')
-        self.std = None
-        self.register_persistent('std')
+        self.max_abs = None
+        self.register_persistent('max_abs')
 
     def fit(self, x, indices=None, axis=1):
         """Fitting parameter.
@@ -54,46 +52,38 @@ class MaxAbsScaler(BaseScaler):
         xp = self.xp
         if xp is numpy:
             x = cuda.to_cpu(x)
-            self.mean = xp.nanmean(x, axis=0)
-            self.std = xp.nanstd(x, axis=0)
         else:
             x = cuda.to_gpu(x)
-            if int(xp.sum(xp.isnan(x))) > 0:
-                raise NotImplementedError(
-                    "StandardScaling with nan value on GPU is not supported.")
-            # cupy.nanmean, cupy.nanstd is not implemented yet.
-            self.mean = xp.mean(x, axis=0)
-            self.std = xp.std(x, axis=0)
+        self.max_abs = xp.nanmax(xp.abs(x), axis=0)
 
         # result consistency check
-        if xp.sum(self.std == 0) > 0:
+        if xp.sum(self.max_abs == 0) > 0:
             logger = getLogger(__name__)
-            ind = numpy.argwhere(cuda.to_cpu(self.std) == 0)[:, 0]
-            logger.warning('fit: std was 0 at indices {}'.format(ind))
+            ind = numpy.argwhere(cuda.to_cpu(self.max_abs) == 0)[:, 0]
+            logger.warning('fit: max_abs was 0 at indices {}'.format(ind))
         return self
 
-    def _compute_mean_std_all(self, input_dim):
+    def _compute_max_abs_all(self, input_dim):
         if self.indices is None:
-            std_all = self.xp.ones(input_dim, dtype=self.xp.float32)
-            std_all[self.std != 0] = self.std[self.std != 0]
-            return self.mean, std_all
+            max_abs_all = self.xp.ones(input_dim, dtype=self.xp.float32)
+            max_abs_all[self.max_abs != 0] = self.max_abs[self.max_abs != 0]
+            return max_abs_all
         else:
-            mean_all = self.xp.zeros(input_dim, dtype=self.xp.float32)
-            mean_all[self.indices] = self.mean
-            std_all = self.xp.ones(input_dim, dtype=self.xp.float32)
-            non_zero_indices = self.indices[self.std != 0]
-            std_all[non_zero_indices] = self.std[self.std != 0]
-            return mean_all, std_all
+            max_abs_all = self.xp.ones(input_dim, dtype=self.xp.float32)
+            non_zero_indices = self.indices[self.max_abs != 0]
+            max_abs_all[non_zero_indices] = self.max_abs[self.max_abs != 0]
+            return max_abs_all
 
     def transform(self, x, axis=1):
         is_array = not isinstance(x, Variable)
-        if self.mean is None:
-            raise AttributeError('[Error] mean is None, call fit beforehand!')
+        if self.max_abs is None:
+            raise AttributeError(
+                '[Error] max_abs is None, call fit beforehand!')
         x = format_x(x)
         shape_transformer = ShapeTransformer2D(axis=axis)
         x = shape_transformer.transform(x)
-        mean_all, std_all = self._compute_mean_std_all(x.shape[1])
-        x = (x - mean_all[None, :]) / std_all[None, :]
+        max_abs_all = self._compute_max_abs_all(x.shape[1])
+        x = x / max_abs_all[None, :]
         x = shape_transformer.inverse_transform(x)
         if is_array:
             x = x.array
@@ -101,13 +91,14 @@ class MaxAbsScaler(BaseScaler):
 
     def inverse_transform(self, x, axis=1):
         is_array = not isinstance(x, Variable)
-        if self.mean is None:
-            raise AttributeError('[Error] mean is None, call fit beforehand!')
+        if self.max_abs is None:
+            raise AttributeError(
+                '[Error] max_abs is None, call fit beforehand!')
         x = format_x(x)
         shape_transformer = ShapeTransformer2D(axis=axis)
         x = shape_transformer.transform(x)
-        mean_all, std_all = self._compute_mean_std_all(x.shape[1])
-        x = x * std_all[None, :] + mean_all[None, :]
+        max_abs_all = self._compute_max_abs_all(x.shape[1])
+        x = x * max_abs_all[None, :]
         x = shape_transformer.inverse_transform(x)
         if is_array:
             x = x.array
