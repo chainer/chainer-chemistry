c8ea674bc2e5d41602d7e3084735fda97fc24857
corochann <acc1ssnn9terias@gmail.com>
2019-08-08 16:49:40 +0900

    add tests

--- chainer_chemistry/models/prediction/set_up_predictor.py
+++ chainer_chemistry/models/prediction/set_up_predictor.py
@@ -5,6 +5,7 @@ from typing import Optional  # NOQA
 import chainer  # NOQA
 
 from chainer_chemistry.models.ggnn import GGNN
+from chainer_chemistry.models.gin import GIN
 from chainer_chemistry.models.mlp import MLP
 from chainer_chemistry.models.nfp import NFP
 from chainer_chemistry.models.prediction.graph_conv_predictor import GraphConvPredictor  # NOQA
