9220e2b2e1850a983be7cb0166ed358f1d782b5e
Hirotaka Akita <akita714@preferred.jp>
2019-09-18 15:31:50 +0900

    add shift to all softplus and avoid overflow

--- chainer_chemistry/functions/activation/shifted_softplus.py
+++ chainer_chemistry/functions/activation/shifted_softplus.py
@@ -2,14 +2,19 @@ import chainer
 from chainer import functions
 
 
-def shifted_softplus(x, beta=1, shift=0.5):
+def shifted_softplus(x, beta=1, shift=0.5, threshold=20):
     """shifted softplus function, which holds f(0)=0.
 
      Args:
         x (Variable): Input variable
+        beta (float): Parameter :math:`\\beta`.
+        shift (float): Shift Parameter 
+        threshold (float): threshold to avoid overflow
 
     Returns:
         output (Variable): Output variable whose shape is same with `x`
     """
     xp = chainer.cuda.get_array_module(x)
-    return functions.softplus(x, beta=beta) + xp.log(shift)
+    x = functions.where(x > threshold, x, functions.softplus(x, beta=beta))
+    x += xp.log(shift)
+    return x
--- tests/functions_tests/activation/test_shifted_softplus.py
+++ tests/functions_tests/activation/test_shifted_softplus.py
@@ -25,6 +25,13 @@ def test_forward_zero_cpu():
     numpy.allclose(output.array, expect_output)
 
 
+def test_forward_avoid_overflow_cpu():
+    x = numpy.array([1e5], dtype=numpy.float32)
+    output = shifted_softplus(x)
+    expect_output = numpy.array([1e5], dtype=numpy.float32)
+    numpy.allclose(output.array, expect_output)
+
+
 @pytest.mark.gpu
 def test_forward_gpu():
     x = cuda.to_gpu(numpy.array([[1, 2, 3], [6, 5, 4]], dtype=numpy.float32))
@@ -40,6 +47,14 @@ def test_forward_zero_gpu():
     x = cuda.to_gpu(numpy.zeros((2, 3), dtype=numpy.float32))
     output = shifted_softplus(x)
     expect_output = numpy.zeros((2, 3), dtype=numpy.float32)
+    numpy.allclose(cuda.to_cpu(output.array), expect_output)
+
+
+@pytest.mark.gpu
+def test_forward_avoid_overflow_gpu():
+    x = cuda.to_gpu(numpy.array([1e5], dtype=numpy.float32))
+    output = shifted_softplus(x)
+    expect_output = numpy.array([1e5], dtype=numpy.float32)
     numpy.allclose(cuda.to_cpu(output.array), expect_output)
 
 
